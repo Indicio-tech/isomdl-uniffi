2db07fb7f580bffd70a1dc0a5bfc393d26bebbad
Adam Burdett <burdettadam@gmail.com>
2025-11-14 16:23:28 -0700

    Add appropriate license headers to source files (#7)
    - Rust files: Include SpruceID attribution as they use/wrap isomdl library
    - Python/Kotlin files: Indicio-only headers as these are original wrapper code
    - Both use dual licensing (Apache-2.0 OR MIT) with SPDX identifiers
    - Added license templates for future consistency:
      - license-template-with-spruceid.txt: For files using SpruceID code
      - license-template-indicio.txt: For original Indicio code
    - Properly distinguishes between original and derived work
    
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- rust/src/mdl/holder.rs
+++ rust/src/mdl/holder.rs
@@ -1,3 +1,14 @@
+// Copyright (c) 2025 Indicio
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+// This software may be modified and distributed under the terms
+// of either the Apache License, Version 2.0 or the MIT license.
+// See the LICENSE-APACHE and LICENSE-MIT files for details.
+//
+// This project contains code from Spruce Systems, Inc.
+// (https://github.com/spruceid/isomdl)
+// Copyright (c) 2022 Spruce Systems, Inc.
+
 //https://github.com/spruceid/sprucekit-mobile/blob/main/rust/src/mdl/holder.rs
 
 use isomdl::definitions::x509::trust_anchor::TrustAnchorRegistry;

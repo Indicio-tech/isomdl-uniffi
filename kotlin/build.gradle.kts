85899ed5b3b08f2bd90639281a940275ad124e8e
Adam Burdett <burdettadam@gmail.com>
2025-11-18 14:10:54 -0700

    Remove copyright headers from Kotlin files
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) 2025 Indicio
- * SPDX-License-Identifier: Apache-2.0 OR MIT
- *
- * This software may be modified and distributed under the terms
- * of either the Apache License, Version 2.0 or the MIT license.
- * See the LICENSE-APACHE and LICENSE-MIT files for details.
- */
-
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 import gobley.gradle.cargo.dsl.jvm
